b1d842f6e52dd67d06c10e8a19e64c2c68573929
AndreSilva <sektor_411@hotmail.com>
2021-06-28 11:03:08 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+## Instalação ##
+
+1. Executar o comando `npm install`, dentro da pasta raiz.
+2. Executar o comando `npm install firebase@4.6.1 angularfire2@5.0.0-rc.3 --save`, para a instalação das dependencias do FIREBASE.
+
+## Como subir a aplicação ##
+
+1. Executar o comando `npm start`, quando compilar 100% navegar para
+`http://localhost:4200/`
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase :{
+    apiKey: "AIzaSyD22n8RtC2R2yA5pugT3IV2dNkXP390Pvc",
+    authDomain: "db-angular2.firebaseapp.com",
+    databaseURL: "https://db-angular2-default-rtdb.firebaseio.com",
+    projectId: "db-angular2",
+    storageBucket: "db-angular2.appspot.com",
+    messagingSenderId: "1002646352368",
+    appId: "1:1002646352368:web:9ab2b298ee4c574cb72d35"
+  }
+};
